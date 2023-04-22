a648a8fbea97951bdf2e13dbeaaf90ac57a51799
Chen Xu <chenxu@chens-mbp.lan>
2023-04-21 23:20:09 -0400

    944. Delete Columns to Make Sorted

---
+++ 944_delete_columns_to_make_sorted.java
@@ -0,0 +1,15 @@
+class Solution {
+    // TC: O(n*m), SC: O(1)
+    public int minDeletionSize(String[] strs) {
+        int res = 0;
+        for (int i = 0; i < strs[0].length(); i++) {
+            for (int j = 1; j < strs.length; j++) {
+                if (strs[j].charAt(i) - strs[j - 1].charAt(i) < 0) {
+                    res++;
+                    break;
+                }
+            }
+        }
+        return res;
+    }
+}
