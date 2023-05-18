7a9b4b83e25b2b69a71359355e2e06f1da4bbb23
Chen Xu <chenxu@Chens-MacBook-Pro.local>
2023-05-18 10:22:16 -0400

    1290. Convert Binary Number in a Linked List to Integer

---
+++ 1290_convert_binary_number_in_a_linked_list_to_integer.java
@@ -0,0 +1,11 @@
+class Solution {
+    // TC: O(n), SC: O(1)
+    public int getDecimalValue(ListNode head) {
+        int res = 0;
+        while (head != null) {
+            res = res * 2 + head.val;
+            head = head.next;
+        }
+        return res;
+    }
+}
