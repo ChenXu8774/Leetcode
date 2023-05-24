67787596329035d425d995f4e2ce8d6b57ebe845
Chen Xu <chenxu@chens-mbp.lan>
2023-05-24 09:02:56 -0400

    112. Path Sum

---
+++ 112_path_sum.java
@@ -0,0 +1,12 @@
+class Solution {
+    // TC: O(n), SC: O(h)
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+        if (root == null) {
+            return false;
+        }
+        if (root.left == null && root.right == null) {
+            return root.val == targetSum;
+        }
+        return hasPathSum(root.left, targetSum - root.val) || hasPathSum(root.right, targetSum - root.val);
+    }
+}
