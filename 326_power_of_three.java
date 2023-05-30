2df6656334cfa77b578a8e50a52c6107f121fa7c
Chen Xu <chenxu@chens-mbp.lan>
2023-05-29 23:56:59 -0400

    326. Power of Three

---
+++ 326_power_of_three.java
@@ -0,0 +1,15 @@
+class Solution {
+    // TC: O(log(n)), SC: O(1)
+    public boolean isPowerOfThree(int n) {
+        if (n <= 0) {
+            return false;
+        }
+        while (n > 1) {
+            if (n % 3 != 0) {
+                return false;
+            }
+            n /= 3;
+        }
+        return true;
+    }
+}
