bda28670a8ea5724cd1c16ba52c68551664de4e2
Chen Xu <chenxu@chens-mbp.lan>
2023-06-05 16:46:11 -0400

    872. Leaf-Similar Trees

---
+++ 872_leaf_similar_trees.java
@@ -0,0 +1,20 @@
+class Solution {
+    // TC: O(max(n1, n2)), SC: O(max(h1,h2))
+    public boolean leafSimilar(TreeNode root1, TreeNode root2) {
+        List<Integer> list1 = new ArrayList<>();
+        List<Integer> list2 = new ArrayList<>();
+        helper(root1, list1);
+        helper(root2, list2);
+        return list1.equals(list2);
+    }
+    private void helper(TreeNode node, List<Integer> list) {
+        if (node == null) {
+            return;
+        }
+        if (node.left == null && node.right == null) {
+            list.add(node.val);
+        }
+        helper(node.left, list);
+        helper(node.right, list);
+    }
+}
